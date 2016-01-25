2d8feda44c834b124cd8e51ce3e11f4602a7999e
Tim Freiheit <timfreiheit@hotmail.de>
2016-01-25 20:10:21 +0100

    fixed base class

--- library/src/main/java/de/freiheit/activityresponsedelegate/base/ResponseDelegateAppCompatActivity.java
+++ library/src/main/java/de/freiheit/activityresponsedelegate/base/ResponseDelegateAppCompatActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
 import de.freiheit.activityresponsedelegate.ActivityResponseDelegate;
 
 /**
@@ -11,7 +12,7 @@ import de.freiheit.activityresponsedelegate.ActivityResponseDelegate;
  *
  * Created by timfreiheit on 25.01.16.
  */
-public class ResponseDelegateAppCompatActivity extends Activity {
+public class ResponseDelegateAppCompatActivity extends AppCompatActivity {
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
