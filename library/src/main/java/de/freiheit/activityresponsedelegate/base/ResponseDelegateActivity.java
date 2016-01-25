7a72c70dc618e2faab462e819b752779efb646f5
Tim Freiheit <timfreiheit@hotmail.de>
2016-01-25 17:56:50 +0100

    add useful base classes

---
+++ library/src/main/java/de/freiheit/activityresponsedelegate/base/ResponseDelegateActivity.java
@@ -0,0 +1,43 @@
+package de.freiheit.activityresponsedelegate.base;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import de.freiheit.activityresponsedelegate.ActivityResponseDelegate;
+
+/**
+ * useful base class when using {@link ActivityResponseDelegate}
+ *
+ * Created by timfreiheit on 25.01.16.
+ */
+public class ResponseDelegateActivity extends Activity {
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        // delegate the result handling
+        ActivityResponseDelegate.from(this).onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        // delegate the permission handling
+        ActivityResponseDelegate.from(this).onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        // save the delegate state in case the activity will be destroyed
+        ActivityResponseDelegate.from(this).onSaveInstanceState(outState);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        // restore delegate state
+        ActivityResponseDelegate.from(this).onRestoreInstanceState(savedInstanceState);
+    }
+
+}
---
+++ library/src/main/java/de/freiheit/activityresponsedelegate/base/ResponseDelegateFragment.java
@@ -0,0 +1,41 @@
+package de.freiheit.activityresponsedelegate.base;
+
+import android.app.Fragment;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import de.freiheit.activityresponsedelegate.ActivityResponseDelegate;
+
+/**
+ * useful base class when using {@link ActivityResponseDelegate}
+ *
+ * Created by timfreiheit on 25.01.16.
+ */
+public class ResponseDelegateFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ActivityResponseDelegate.from(this).onRestoreInstanceState(savedInstanceState);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        // delegate the result handling
+        ActivityResponseDelegate.from(this).onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        // delegate the permission handling
+        ActivityResponseDelegate.from(this).onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        ActivityResponseDelegate.from(this).onSaveInstanceState(outState);
+    }
+
+}
